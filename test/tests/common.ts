50f5f0301847466cd5908dc496747a9bdbc3c344
dafuga <danielfugere28@gmail.com>
2024-09-25 18:11:40 -0700

    chore: disabling tests for now

--- test/tests/common.ts
+++ test/tests/common.ts
@@ -1,60 +1,60 @@
-import {assert} from 'chai'
-import {PermissionLevel, SessionKit} from '@wharfkit/session'
+// import {assert} from 'chai'
+// import {PermissionLevel, SessionKit} from '@wharfkit/session'
 
-import {WalletPluginTokenPocket} from '$lib'
-import {mockFetch} from '$test/utils/mock-fetch'
-import {MockStorage} from '$test/utils/mock-storage'
-import {MockUserInterface} from '$test/utils/mock-ui'
+// import {WalletPluginTokenPocket} from '$lib'
+// import {mockFetch} from '$test/utils/mock-fetch'
+// import {MockStorage} from '$test/utils/mock-storage'
+// import {MockUserInterface} from '$test/utils/mock-ui'
 
-const mockChainDefinition = {
-    id: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
-    url: 'https://eos.greymass.com',
-}
+// const mockChainDefinition = {
+//     id: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
+//     url: 'https://eos.greymass.com',
+// }
 
-const mockPermissionLevel = PermissionLevel.from('wharfkit1111@test')
+// const mockPermissionLevel = PermissionLevel.from('wharfkit1111@test')
 
-const mockSessionKitOptions = {
-    appName: 'unittests',
-    chains: [mockChainDefinition],
-    fetch: mockFetch, // Required for unit tests
-    storage: new MockStorage(),
-    ui: new MockUserInterface(),
-    walletPlugins: [new WalletPluginTokenPocket()],
-}
+// const mockSessionKitOptions = {
+//     appName: 'unittests',
+//     chains: [mockChainDefinition],
+//     fetch: mockFetch, // Required for unit tests
+//     storage: new MockStorage(),
+//     ui: new MockUserInterface(),
+//     walletPlugins: [new WalletPluginTokenPocket()],
+// }
 
-suite('wallet plugin', function () {
-    this.timeout(120 * 1000)
-    this.slow(5 * 1000)
+// suite('wallet plugin', function () {
+//     this.timeout(120 * 1000)
+//     this.slow(5 * 1000)
 
-    // test('login and sign', async function () {
+//     // test('login and sign', async function () {
 
-    //     const kit = new SessionKit(mockSessionKitOptions)
-    //     const { session } = await kit.login({
-    //         chain: mockChainDefinition.id,
-    //         permissionLevel: mockPermissionLevel,
-    //     })
-    //     assert.isTrue(session.chain.equals(mockChainDefinition))
-    //     assert.isTrue(session.actor.equals(mockPermissionLevel.actor))
-    //     assert.isTrue(session.permission.equals(mockPermissionLevel.permission))
-    //     const result = await session.transact(
-    //         {
-    //             action: {
-    //                 authorization: [mockPermissionLevel],
-    //                 account: 'eosio.token',
-    //                 name: 'transfer',
-    //                 data: {
-    //                     from: mockPermissionLevel.actor,
-    //                     to: 'wharfkittest',
-    //                     quantity: '0.0001 EOS',
-    //                     memo: 'wharfkit/session wallet plugin template',
-    //                 },
-    //             },
-    //         },
-    //         {
-    //             broadcast: false,
-    //         }
-    //     )
-    //     assert.isTrue(result.signer.equals(mockPermissionLevel))
-    //     assert.equal(result.signatures.length, 1)
-    // })
-})
+//     //     const kit = new SessionKit(mockSessionKitOptions)
+//     //     const { session } = await kit.login({
+//     //         chain: mockChainDefinition.id,
+//     //         permissionLevel: mockPermissionLevel,
+//     //     })
+//     //     assert.isTrue(session.chain.equals(mockChainDefinition))
+//     //     assert.isTrue(session.actor.equals(mockPermissionLevel.actor))
+//     //     assert.isTrue(session.permission.equals(mockPermissionLevel.permission))
+//     //     const result = await session.transact(
+//     //         {
+//     //             action: {
+//     //                 authorization: [mockPermissionLevel],
+//     //                 account: 'eosio.token',
+//     //                 name: 'transfer',
+//     //                 data: {
+//     //                     from: mockPermissionLevel.actor,
+//     //                     to: 'wharfkittest',
+//     //                     quantity: '0.0001 EOS',
+//     //                     memo: 'wharfkit/session wallet plugin template',
+//     //                 },
+//     //             },
+//     //         },
+//     //         {
+//     //             broadcast: false,
+//     //         }
+//     //     )
+//     //     assert.isTrue(result.signer.equals(mockPermissionLevel))
+//     //     assert.equal(result.signatures.length, 1)
+//     // })
+// })
